909b43b22d99dda4330d46d6a17c01ff65c11727
jessicasi <aron.jess@gmail.com>
2021-10-11 14:52:08 -0700

    Material Search working

--- MegaDesk-Stratton/SearchQuotes.Designer.cs
+++ MegaDesk-Stratton/SearchQuotes.Designer.cs
@@ -29,9 +29,12 @@ namespace MegaDesk_Stratton
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.searchQuotesClose = new System.Windows.Forms.Button();
             this.searchLbl = new System.Windows.Forms.Label();
             this.searchMaterialsComboBox = new System.Windows.Forms.ComboBox();
+            this.searchGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.searchGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // searchQuotesClose
@@ -46,10 +49,11 @@ namespace MegaDesk_Stratton
             // 
             // searchLbl
             // 
-            this.searchLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.searchLbl.Location = new System.Drawing.Point(54, 38);
+            this.searchLbl.AutoSize = true;
+            this.searchLbl.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchLbl.Location = new System.Drawing.Point(228, 39);
             this.searchLbl.Name = "searchLbl";
-            this.searchLbl.Size = new System.Drawing.Size(150, 25);
+            this.searchLbl.Size = new System.Drawing.Size(194, 23);
             this.searchLbl.TabIndex = 1;
             this.searchLbl.Text = "Search By Material: ";
             this.searchLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -57,12 +61,39 @@ namespace MegaDesk_Stratton
             // searchMaterialsComboBox
             // 
             this.searchMaterialsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.searchMaterialsComboBox.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.searchMaterialsComboBox.FormattingEnabled = true;
-            this.searchMaterialsComboBox.Location = new System.Drawing.Point(210, 38);
+            this.searchMaterialsComboBox.Location = new System.Drawing.Point(452, 38);
             this.searchMaterialsComboBox.Name = "searchMaterialsComboBox";
-            this.searchMaterialsComboBox.Size = new System.Drawing.Size(121, 21);
-            this.searchMaterialsComboBox.TabIndex = 2;
+            this.searchMaterialsComboBox.Size = new System.Drawing.Size(121, 28);
+            this.searchMaterialsComboBox.TabIndex = 1;
             this.searchMaterialsComboBox.SelectedIndexChanged += new System.EventHandler(this.searchMaterialsComboBox_SelectedIndexChanged);
+            // 
+            // searchGrid
+            // 
+            this.searchGrid.AllowUserToAddRows = false;
+            this.searchGrid.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Ivory;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(1);
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.searchGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.searchGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.searchGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.searchGrid.Location = new System.Drawing.Point(12, 106);
+            this.searchGrid.Name = "searchGrid";
+            this.searchGrid.RowHeadersVisible = false;
+            this.searchGrid.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.searchGrid.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.searchGrid.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchGrid.RowTemplate.DefaultCellStyle.Format = "d";
+            this.searchGrid.RowTemplate.DefaultCellStyle.NullValue = null;
+            this.searchGrid.RowTemplate.DefaultCellStyle.Padding = new System.Windows.Forms.Padding(1);
+            this.searchGrid.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.searchGrid.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.searchGrid.Size = new System.Drawing.Size(776, 274);
+            this.searchGrid.TabIndex = 3;
             // 
             // SearchQuotes
             // 
@@ -70,6 +101,7 @@ namespace MegaDesk_Stratton
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::MegaDesk_Stratton.Properties.Resources.woodgrain_texture_background;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchGrid);
             this.Controls.Add(this.searchMaterialsComboBox);
             this.Controls.Add(this.searchLbl);
             this.Controls.Add(this.searchQuotesClose);
@@ -78,7 +110,9 @@ namespace MegaDesk_Stratton
             this.Name = "SearchQuotes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Search Quotes";
+            ((System.ComponentModel.ISupportInitialize)(this.searchGrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -87,5 +121,6 @@ namespace MegaDesk_Stratton
         private System.Windows.Forms.Button searchQuotesClose;
         private System.Windows.Forms.Label searchLbl;
         private System.Windows.Forms.ComboBox searchMaterialsComboBox;
+        private System.Windows.Forms.DataGridView searchGrid;
     }
 }
--- MegaDesk-Stratton/SearchQuotes.cs
+++ MegaDesk-Stratton/SearchQuotes.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,9 @@ namespace MegaDesk_Stratton
 {
     public partial class SearchQuotes : Form
     {
+        private string JsonQuotes = @"Data\quotes.json";
+        private static DesktopMaterial material;
+
         public SearchQuotes()
         {
             InitializeComponent();
@@ -28,7 +33,35 @@ namespace MegaDesk_Stratton
 
         private void searchMaterialsComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            material = (DesktopMaterial)searchMaterialsComboBox.SelectedItem;
+            FindQuotes(material);
         }
+
+        private void FindQuotes(DesktopMaterial material)
+        {
+            
+            //Error Checking Needed
+            var jsonData = File.ReadAllText(JsonQuotes);
+            var searchData = JsonConvert.DeserializeObject<List<DeskQuote>>(jsonData);
+            List<DeskQuote> searchResults = searchData.FindAll(FindMaterial);
+            searchGrid.DataSource = searchResults;
+            searchGrid.AutoResizeColumns();
+            searchGrid.RowHeadersVisible = false;
+            searchGrid.BackgroundColor = Color.AntiqueWhite;
+            searchGrid.GridColor = Color.AntiqueWhite;
+        }
+
+        private static bool FindMaterial(DeskQuote dk)
+        {
+            if (dk.Desk.desktopMaterial == material)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+
+        
     }
 }
