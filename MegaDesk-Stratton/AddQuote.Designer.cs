30fa9d5b3c005bd2cdd833dc810bc2a571b69c07
Heather Stratton <hastratton@gmail.com>
2021-10-14 17:44:01 -0400

    added back combobox info

--- MegaDesk-Stratton/AddQuote.Designer.cs
+++ MegaDesk-Stratton/AddQuote.Designer.cs
@@ -169,6 +169,16 @@ namespace MegaDesk_Stratton
             this.desktopMatComboBox.ForeColor = System.Drawing.Color.DarkSlateGray;
             this.desktopMatComboBox.FormattingEnabled = true;
             this.desktopMatComboBox.ItemHeight = 21;
+            ComboboxItem laminate = new ComboboxItem("Laminate", DesktopMaterial.Laminate);
+            ComboboxItem oak = new ComboboxItem("Oak", DesktopMaterial.Oak);
+            ComboboxItem rosewood = new ComboboxItem("Rosewood", DesktopMaterial.Rosewood);
+            ComboboxItem veneer = new ComboboxItem("Veneer", DesktopMaterial.Veneer);
+            ComboboxItem pine = new ComboboxItem("Pine", DesktopMaterial.Pine);
+            this.desktopMatComboBox.Items.Add(laminate);
+            this.desktopMatComboBox.Items.Add(oak);
+            this.desktopMatComboBox.Items.Add(rosewood);
+            this.desktopMatComboBox.Items.Add(veneer);
+            this.desktopMatComboBox.Items.Add(pine);
             this.desktopMatComboBox.Location = new System.Drawing.Point(209, 198);
             this.desktopMatComboBox.Name = "desktopMatComboBox";
             this.desktopMatComboBox.Size = new System.Drawing.Size(150, 29);
